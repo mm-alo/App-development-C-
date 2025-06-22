96483fc037cbb992960e3f6d15ef324ec4440db5
emmanuelalo209@gmail.com <emmanuelalo209@gmail.com>
2025-06-22 10:05:36 +0800

    tagos ni nar

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a = 0;
+int b = 3;
+int result = a + b;
+Console.WriteLine("What is your year level? ", result);
